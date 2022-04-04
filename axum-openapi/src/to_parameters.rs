0e3ee53be626ca7471fce92165bb9c8e9c802f4d
Max Bruckner <max@maxbruckner.de>
2022-04-04 02:43:24 +0200

    WIP

--- axum-openapi/src/to_parameters.rs
+++ axum-openapi/src/to_parameters.rs
@@ -8,3 +8,6 @@ pub enum Parameter {
     /// Put the parameter in the components and reference it.
     Components(String, okapi::openapi3::Parameter),
 }
+
+// FIXME: Parameters need to differentiate between "path", "query", "header", "cookie" and "requestContent"
+// (the last one doesn't exist in OpenAPI parameters, but they are used as parameters in axum anyways.
